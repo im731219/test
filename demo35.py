2c2f69da96412f7ed557d71819b9e6f11752b3c7
im731219 <im731219@gmail.com>
2019-09-04 11:17:30 +0800

    ver1

---
+++ demo35.py
@@ -0,0 +1,12 @@
+l1 = list("QWERTYUIOOUYTREWERUPIUYTWERTYUI")
+print(l1)
+
+counts = {}
+print(type(counts))
+for l in l1:
+    counts.setdefault(l, 0)
+    counts[l] = counts[l] + 1
+
+
+for key, count in counts.items():
+    print("[%s] count=%d" % (key, count))
